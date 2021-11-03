251a4f90eb40faa62c5f1b92c1154e28acde462f
RyuZU <ryuzu4510@icloud.com>
2021-11-04 00:07:42 +0900

    EnemyAI

--- Assets/ChaseBehavior.cs
+++ Assets/ChaseBehavior.cs
@@ -19,14 +19,14 @@ public class ChaseBehavior : StateMachineBehaviour
     {
         agent.SetDestination(player.position);
         float distance = Vector3.Distance(animator.transform.position, player.position);
-        if (distance < 7)
+        if (distance < 10)
             animator.SetBool("isAttacking", true);
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        agent.SetDestination(agent.transform.position);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
